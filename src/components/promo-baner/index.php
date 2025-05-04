89e2aaf2f86ddeee3d8144bd8cac93ed7ea66321
d.andreev <dimaandreev0101@gmail.com>
2025-05-04 20:23:23 +0300

    add: promo banner

--- src/components/promo-baner/index.php
+++ src/components/promo-baner/index.php
@@ -2,35 +2,21 @@
   <link rel="stylesheet" href="/src/components/promo-baner/index.css" />
   <base-slider class="promo-baner__slider" height="170" infinity shrink="50" gap="10" autoplay>
     <?php include 'src/components/base-slider/index.php' ?>
+    <?php $imgUrls = [
+      '/src/shared/assets/banners/2880x336_1.webp',
+      '/src/shared/assets/banners/spring_2880.webp',
+      '/src/shared/assets/banners/remont_2880.webp',
+      '/src/shared/assets/banners/local_storage_by_2880.webp',
+      '/src/shared/assets/banners/minsk-dost_2880.webp',
+      '/src/shared/assets/banners/2880dacha1104.webp'
+    ];
+
+    foreach ($imgUrls as $imgUrl):
+    ?>
     <slider-item class="slide-item">
       <?php include 'src/components/slider-item/index.php' ?>
-      <img src="/src/shared/assets/banners/2880x336_1.webp" alt="" />
+      <img loading="lazy" src="<?php echo $imgUrl ?>" alt="" />
     </slider-item>
-
-    <slider-item class="slide-item">
-      <?php include 'src/components/slider-item/index.php' ?>
-      <img src="/src/shared/assets/banners/spring_2880.webp" alt="" />
-    </slider-item>
-
-    <slider-item class="slide-item">
-      <?php include 'src/components/slider-item/index.php' ?>
-      <img src="/src/shared/assets/banners/remont_2880.webp" alt="" />
-    </slider-item>
-
-    <slider-item class="slide-item">
-      <?php include 'src/components/slider-item/index.php' ?>
-      <img src="/src/shared/assets/banners/local_storage_by_2880.webp" alt="" />
-    </slider-item>
-
-    <slider-item class="slide-item">
-      <?php include 'src/components/slider-item/index.php' ?>
-      <img src="/src/shared/assets/banners/minsk-dost_2880.webp" alt="" />
-    </slider-item>
-
-    <slider-item class="slide-item">
-      <?php include 'src/components/slider-item/index.php' ?>
-      <img src="/src/shared/assets/banners/2880dacha1104.webp" alt="" />
-    </slider-item>
-
+    <?php endforeach; ?>
   </base-slider>
 </template>
